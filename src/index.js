af51c983ba81947f0f26738db1823371803a3fa4
Mulyono Putra <mulyonoputra27@outlook.com>
2022-05-13 00:10:16 +0700

    initial commit with install and setup redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import { createStore, compose, applyMiddleware } from 'redux';
+
+import { Provider } from 'react-redux';
+
+import thunk from '../node_modules/redux-thunk/es/index';
+
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+import reducers from './reducers/index';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import ContactReducer from './contact-reducer/ContactReducer';
+
+export default combineReducers({
+  ContactReducer,
+});
